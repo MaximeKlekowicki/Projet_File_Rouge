1ae60893f11b301234c6acf74135653c9f135101
Klekowicki <maxime.klekowicki@gmail.com>
2019-06-21 17:00:24 +0200

    Interface des Attaques

---
+++ Projet_File_Rouge/Interface/IAttaque.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Projet_File_Rouge.Classe;
+
+namespace Projet_File_Rouge.Interface
+{
+    interface IAttaque
+    {
+        ClassePersonnage Attaque(ClassePersonnage perso);
+        ClassePersonnage AttaqueSpecial(ClassePersonnage perso);
+
+    }
+}
